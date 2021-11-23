bc07a8bbdc3860d6ce6e3eaefb86b2de363de58a
sun <2870926114@qq.com>
2021-11-23 19:13:07 +0800

    2021/11/23

--- 买卖股票的最佳时机.java
+++ 买卖股票的最佳时机.java
@@ -1,13 +1,18 @@
 public class 买卖股票的最佳时机 {
-    public int maxProfit(int[] prices) {
-        int profit = 0,min = 0;
+    public static int maxProfit(int[] prices) {
+        int profit = 0;
 
         if (prices.length < 2)
             return 0;
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > prices[i-1])
-
+                profit += prices[i]-prices[i-1];
         }
-        return 0;
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int[] arr ={7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
     }
 }
