62a44e70f8e70dd7bf40d82f794eb3ae5eba11ee
sun <2870926114@qq.com>
2021-11-24 21:13:33 +0800

    2021/11/24---旋转数组===out

--- daily/旋转数组.java
+++ daily/旋转数组.java
@@ -1,9 +1,26 @@
 package daily;
 
 public class 旋转数组 {
+    private static int[] next(int[] arrs,int k)
+    {
+        if (k>0){
+            int trans = arrs[arrs.length-1];
+            for (int i = arrs.length-1; i > 0 ; i--) {
+                arrs[i] =arrs[i--];
+            }
+            arrs[0] = trans;
+            return next(arrs,k--);
+        }
+        else return arrs;
+    }
     public void rotate(int[] nums, int k) {
-        int trans;
+        //空间O(1)
+//        System.out.println(next(nums,k);
 
+    }
 
+    public static void main(String[] args) {
+        int[] arrat={1,2,3,4,5,6,7};
+        System.out.println(next(arrat,3));
     }
 }
